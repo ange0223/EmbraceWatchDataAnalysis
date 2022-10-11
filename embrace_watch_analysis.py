2c681477363eb3115a9d170612c07dec32b351c0
ange0223 <ahall@eventpower.com>
2022-10-11 16:11:33 -0400

    adding basic GUI holder

--- embrace_watch_analysis.py
+++ embrace_watch_analysis.py
@@ -1,9 +1,10 @@
-import pandas as pd
+#import pandas
+#import random
+#from datetime import datetime
+#from pytz import timezone
 import tkinter as tk
 from tkinter import ttk
-import random
-from datetime import datetime
-from pytz import timezone
+from tkinter import Menu
 
 fmt = "%Y-%m-%d %H:%M:%S"
 
@@ -17,28 +18,60 @@ class WatchData:
     def _init_(self, id, person_id, date_time_utc, current_time_zone, avg_magnitude, avg_eda, avg_temp, movement_intensity, steps, rest, on_wrist):
         self.id = random.random()
 
-    def GetBtwn(date_time_start, date_time_end):
+    # def GetBtwn(date_time_start, date_time_end):
         # Returns data between two DateTimes. If no arguments given, returns the entire dataset.
 
-    def GetPersonData(id):
+    # def GetPersonData(person_id):
         # Returns data from a specific Person 
     
-    def GetPeopleData(id_arr):
+    # def GetPeopleData(id_arr):
         # Returns data from several people 
 
-    def UpdateDisplayTimeZone(self, timezone):
+    # def UpdateDisplayTimeZone(self, timezone):
         # Updates Display Timezone field with the currently selected timezone.
 
-    def GraphData():
+    # def GraphData():
         # Graphs the data to a line graph. 
     
-    def Analyze():
+    # def Analyze():
         # Readies the user selected data for viewing in the Analysis window
 
 class User:
     def _init_(self):
         self.id = random.random()
-        self.local_time_zone =
+        # self.local_time_zone =
 
-    def GetLocalTimeZone():
+    # def GetLocalTimeZone():
         # Gets the local time zone for the current user based on the system the software is currently running on.
+
+# GUI for Our project
+# Main settings for frame
+root = tk.Tk()
+root.title("EmbraceWatch Data Analysis")
+root.geometry('900x600+50+50')
+root.resizable(False, False)
+
+# create a menubar
+menubar = Menu(root)
+root.config(menu=menubar)
+
+# create a menu
+file_menu = Menu(menubar)
+
+# add the File menu to the menubar
+menubar.add_cascade(
+    label="File",
+    menu=file_menu
+)
+
+# add a menu item to the menu
+file_menu.add_command(
+    label='Exit',
+    command=root.destroy
+)
+
+# specifics of what is shown
+ttk.Label(root, text='Themed Label').pack()
+ttk.Button(root, text='Themed Label').pack()
+
+root.mainloop()
