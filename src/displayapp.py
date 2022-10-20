3d1f68d4359c643a76cf4552824bcbf5580db600
Jacob Sanders <sanders.j4@gmail.com>
2022-10-20 16:55:20 -0400

    Added figure generation and scrollbar

--- src/displayapp.py
+++ src/displayapp.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, Menu
+from tkinter.constants import *
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -11,6 +12,24 @@ from datetime import datetime
 from data import load_data
 
 DEFAULT_DATA_PATH = 'Dataset'
+
+
+class ScrollableLabelFrame(ttk.LabelFrame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        canvas = tk.Canvas(self)
+        scrollbar = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
+        self.scrollable_frame = ttk.Frame(canvas)
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        canvas.configure(yscrollcommand=scrollbar.set)
+        canvas.pack(side="left", fill="both", expand=True)
+        scrollbar.pack(side="right", fill="y")
 
 
 class DisplayApp(tk.Tk):
@@ -44,19 +63,35 @@ class DisplayApp(tk.Tk):
         menubar.add_cascade(label='Time Series', menu=time_series_menu)
         menubar.add_cascade(label='Analysis', menu=analysis_menu)
 
-        # placeholder graph
-        lf = ttk.Labelframe(self, text='2020-01-22 to 2020-02-22')
-        lf.grid(row=1, column=0, sticky='nwes', padx=5, pady=5)
-        f = Figure(figsize=(5,4), dpi=100)
-        a = f.add_subplot(111)
-        t = np.arange(0.0, 2.0, 0.01)
-        s = np.sin(2*np.pi*t)
-        a.plot(t, s)
-        data_plot = FigureCanvasTkAgg(f, master=lf)
-        #data_plot.show()
-        data_plot.draw()
-        data_plot.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+        # Todo: range selector
+        range_frame = ttk.Frame(self)
+        range_frame.pack(fill=BOTH, side=TOP)
+        btn = ttk.Button(range_frame, text='Placeholder')
+        btn.pack()
 
+        frame = ScrollableLabelFrame(self, text='2020-01-01 to 2021-01-01')
+        frame.pack(fill=BOTH, expand=True, side=BOTTOM)
+
+        # Get column names to show
+        figure_cols = set(self.data.columns)
+        ignore_cols = {'Datetime (UTC)', 'Timezone (minutes)',
+                'Unix Timestamp (UTC)', 'subject_id'}
+        figure_cols = figure_cols - ignore_cols
+
+        # Only displaying figures for first subject for now
+        subject_id = self.data['subject_id'].unique()[0]
+        subject = self.data[self.data['subject_id'] == subject_id]
+
+        fig_size = (9, 4)
+        fig_dpi = 100
+        for col_name in sorted(figure_cols):
+            subject = subject.astype({'On Wrist': 'int'})
+            fig = Figure(figsize=fig_size, dpi=fig_dpi)
+            ax = fig.add_subplot(111)
+            subject.plot(x='Datetime (UTC)', y=col_name, ax=ax)
+            data_plot = FigureCanvasTkAgg(fig, master=frame.scrollable_frame)
+            data_plot.draw()
+            data_plot.get_tk_widget().pack(fill=X, expand=True)
 
 
 
@@ -66,12 +101,12 @@ if __name__ == '__main__':
         'users': [310, 311],
         'start_time': datetime.strptime('2020-01-17 23:48:00', date_fmt),
         'end_time': datetime.strptime('2022-01-17 23:48:00', date_fmt),
-        'utc_mode': 0,
-        'show_acc': 0,
-        'show_eda': 0,
-        'show_movement': 0,
-        'show_step': 0,
-        'show_wrist': 0
+        'utc_mode': 1,
+        'show_acc': 1,
+        'show_eda': 1,
+        'show_movement': 1,
+        'show_step': 1,
+        'show_wrist': 1
     }
     app = DisplayApp(options)
     print('\napp.data.info():')
