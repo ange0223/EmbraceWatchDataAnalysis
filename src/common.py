a0d7014cdc958657afcd9c8559507c5d591a9010
Jacob Sanders <sanders.j4@gmail.com>
2022-11-16 16:10:08 -0500

    Moved common widgets to common module

---
+++ src/common.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class ScrollableLabelFrame(ttk.LabelFrame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        canvas = tk.Canvas(self)
+        scrollbar = ttk.Scrollbar(self, orient='vertical', command=canvas.yview)
+        self.scrollable_frame = ttk.Frame(canvas)
+        # Configure event triggered anytime the window is changed or mouse
+        # clicked
+        self.scrollable_frame.bind(
+            '<Configure>',
+            lambda e: canvas.configure(
+                # set canvas scroll region to the bounding box of all items
+                # within the canvas
+                scrollregion=canvas.bbox('all')
+            )
+        )
+        canvas.create_window((0, 0), window=self.scrollable_frame, anchor='nw')
+        canvas.configure(yscrollcommand=scrollbar.set)
+        canvas.pack(side='left', fill='both', expand=True)
+        scrollbar.pack(side='right', fill='y')
--- src/displayapp.py
+++ src/displayapp.py
@@ -11,30 +11,9 @@ from datetime import datetime
 
 from data import load_data, get_subject_ids
 from importwindow import ImportWindow
+from common import ScrollableLabelFrame
 
 DEFAULT_DATA_PATH = 'Dataset'
-
-
-class ScrollableLabelFrame(ttk.LabelFrame):
-    def __init__(self, parent, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
-        canvas = tk.Canvas(self)
-        scrollbar = ttk.Scrollbar(self, orient='vertical', command=canvas.yview)
-        self.scrollable_frame = ttk.Frame(canvas)
-        # Configure event triggered anytime the window is changed or mouse
-        # clicked
-        self.scrollable_frame.bind(
-            '<Configure>',
-            lambda e: canvas.configure(
-                # set canvas scroll region to the bounding box of all items
-                # within the canvas
-                scrollregion=canvas.bbox('all')
-            )
-        )
-        canvas.create_window((0, 0), window=self.scrollable_frame, anchor='nw')
-        canvas.configure(yscrollcommand=scrollbar.set)
-        canvas.pack(side='left', fill='both', expand=True)
-        scrollbar.pack(side='right', fill='y')
 
 
 class DataMenu(Menu):
