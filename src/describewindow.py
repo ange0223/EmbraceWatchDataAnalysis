d51d9ac11331d81cb946efdccd240efceb64bf5a
Jacob Sanders <sanders.j4@gmail.com>
2022-11-17 17:57:16 -0500

    Added SourceFrame

--- src/describewindow.py
+++ src/describewindow.py
@@ -5,11 +5,18 @@ from tkinter.constants import *
 from common import ToplevelWindow
 
 
+class SourceFrame(ttk.LabelFrame):
+    def __init__(self, *args, text='Source', **kwargs):
+        super().__init__(*args, text=text, **kwargs)
+        
+
+    def pack(self, fill=BOTH, expand=True, side=TOP):
+        super().pack(fill=fill, expand=expand, side=side)
+
+
 class DescribeWindow(tk.Toplevel):
     def __init__(self, data):
         super().__init__()
         self.data = data
         self.title('Description')
         self.geometry('500x600+100+100')
-
-        
