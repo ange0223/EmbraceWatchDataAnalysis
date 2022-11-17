d128012ba43192d56f00e25e40107f0e57cb6075
Jacob Sanders <sanders.j4@gmail.com>
2022-11-17 17:58:13 -0500

    Added DescriptionFrame

--- src/describewindow.py
+++ src/describewindow.py
@@ -8,7 +8,14 @@ from common import ToplevelWindow
 class SourceFrame(ttk.LabelFrame):
     def __init__(self, *args, text='Source', **kwargs):
         super().__init__(*args, text=text, **kwargs)
-        
+
+    def pack(self, fill=BOTH, expand=True, side=TOP):
+        super().pack(fill=fill, expand=expand, side=side)
+
+
+class DescriptionFrame(ttk.LabelFrame):
+    def __init__(self, *args, text='Source', **kwargs):
+        super().__init__(*args, text=text, **kwargs)
 
     def pack(self, fill=BOTH, expand=True, side=TOP):
         super().pack(fill=fill, expand=expand, side=side)
