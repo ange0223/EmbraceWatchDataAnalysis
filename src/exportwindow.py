e6eacdfe90cb6ed72da0607e581c754171c3f40f
Jacob Sanders <sanders.j4@gmail.com>
2022-11-25 06:01:37 -0500

    Fixed formatting and removed unused imports

--- src/exportwindow.py
+++ src/exportwindow.py
@@ -1,12 +1,5 @@
-import tkinter as tk
-from tkinter import ttk, Menu
-from tkinter.constants import *
-from tkinter import filedialog
 from tkinter.filedialog import asksaveasfile
-from tkinter.filedialog import askdirectory
-from data import *
 
-from common import ToplevelWindow
 
 def open_save_dialog(df):
     save_path = asksaveasfile(mode='a', filetypes=[('CSV', '*.csv')],
